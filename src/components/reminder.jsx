3460b9da49ae11203c53de603e644e20a5c9f4cd
ChrisKim <1515748548@qq.com>
2022-11-11 23:59:33 +0800

    [refactor]: 将便签数据提取到外层

--- src/components/reminder.jsx
+++ src/components/reminder.jsx
@@ -3,31 +3,27 @@ import './reminder.css'
 
 class Reminder extends Component {
     state = {
-        reminderData: [],
         inputTitle: "",
         inputContent: "",
     }
 
+    // 获取输入框标题数据
     inputTitleChange = (val) => {
         this.setState({ inputTitle: val.target.value });
     }
 
+    // 获取输入框内容数据
     inputContentChange = (val) => {
         this.setState({ inputContent: val.target.value });
     }
 
-    handleSave = () => {
-        let reminderData = [...this.state.reminderData];
-        reminderData.push({
-            id: reminderData.length + 1,
-            title: this.state.inputTitle,
-            content: this.state.inputContent,
-            date: "awa!",
-        })
-        this.setState({ reminderData })
+    // 调用外层函数保存便签数据
+    handleAdd = () => {
+        this.props.handleAdd(this.state.inputTitle, this.state.inputContent)
         this.handleReset()
     }
 
+    // 重置输入框
     handleReset = () => {
         this.setState({
             inputTitle: "",
@@ -41,33 +37,33 @@ class Reminder extends Component {
                 <table className="table table-striped table-bordered table-hover">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
+                            <th scope="col">ID</th>
                             <th scope="col">标题</th>
                             <th scope="col">摘要</th>
                             <th scope="col">添加日期</th>
                         </tr>
                     </thead>
-                    <tbody> {this.state.reminderData.map(x =>
+                    <tbody> {this.props.reminderData.map(x =>
                         <tr key={x.id}>
                             <th scope="row">{x.id}</th>
                             <td>{x.title}</td>
                             <td>{x.content}</td>
-                            <td>{x.date}</td>
+                            <td>{x.date.format('YYYY-MM-DD HH:mm')}</td>
                         </tr>
                     )}
                     </tbody>
                 </table>
                 <h3>添加新便签</h3>
-                <div class="mb-2">
-                    <label for="title" class="form-label">标题</label>
-                    <input type="text" class="form-control" id="title" value={this.state.inputTitle} onChange={(val) => this.inputTitleChange(val)} />
+                <div className="mb-2">
+                    <label htmlFor="title" className="form-label">标题</label>
+                    <input type="text" className="form-control" id="title" value={this.state.inputTitle} onChange={(val) => this.inputTitleChange(val)} />
                 </div>
-                <div class="mb-2">
-                    <label for="content" class="form-label">内容</label>
-                    <textarea class="form-control" id="content" rows="4" value={this.state.inputContent} onChange={(val) => this.inputContentChange(val)}></textarea>
+                <div className="mb-2">
+                    <label htmlFor="content" className="form-label">内容</label>
+                    <textarea className="form-control" id="content" rows="4" value={this.state.inputContent} onChange={(val) => this.inputContentChange(val)}></textarea>
                 </div>
-                <button type="button" class="btn btn-primary m-2" onClick={this.handleSave}>保存</button>
-                <button type="button" class="btn btn-danger m-2" onClick={this.handleReset}>重置</button>
+                <button type="button" className="btn btn-primary m-2" onClick={this.handleAdd}>保存</button>
+                <button type="button" className="btn btn-danger m-2" onClick={this.handleReset}>重置</button>
             </React.Fragment>
         );
     }
--- src/page/note.jsx
+++ src/page/note.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import Reminder from '../components/reminder';
+import dayjs, { Dayjs } from 'dayjs';
+
 
 class Note extends Component {
-    state = {  } 
-    render() { 
-        return (<h1>Note Component</h1>);
+    state = {
+        reminderData: [],
+    }
+
+    // 将备忘录加入数据列表中
+    handleAdd = (title, content) => {
+        const reminderData = [...this.state.reminderData];
+        reminderData.push({
+            id: reminderData.length + 1,
+            title: title,
+            content: content,
+            date: dayjs(),
+        })
+        this.setState({ reminderData })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Note Component</h1>
+                <Reminder
+                    reminderData={this.state.reminderData}
+                    handleAdd={this.handleAdd}
+                />
+            </React.Fragment>
+        );
     }
 }
- 
+
 export default Note;
