73006a3bc52299450d392292daad94f08b0d9d90
ChrisKim <1515748548@qq.com>
2022-11-12 18:20:54 +0800

    [feat]:  导航栏选中效果

--- src/page/navbar.jsx
+++ src/page/navbar.jsx
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import icon from '../static/images/icon.png'
+import { useLocation } from 'react-router';
+import icon from '../static/images/icon.png';
 
 
-class Navbar extends Component {
-    state = {}
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">
-                        <img src={icon} alt="icon" width="25" height="25" className="d-inline-block align-text-top"></img>
-                        &nbsp;提醒事项
-                    </Link>
-                    <div className="collapse navbar-collapse">
-                        <div className="navbar-nav">
-                            <Link className="nav-link active" aria-current="page" to="/">首页</Link>
-                            <Link className="nav-link" to="/todo">代办</Link>
-                            <Link className="nav-link" to="/note">便签</Link>
-                        </div>
+const Navbar = () => {
+    const currentPath = useLocation().pathname;
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <img src={icon} alt="icon" width="25" height="25" className="d-inline-block align-text-top"></img>
+                    &nbsp;提醒事项
+                </Link>
+                <div className="collapse navbar-collapse">
+                    <div className="navbar-nav">
+                        <Link className={`nav-link ${currentPath === "/" ? "active" : ""}`} aria-current="page" to="/">首页</Link>
+                        <Link className={`nav-link ${currentPath === "/todo" ? "active" : ""}`} to="/todo">代办</Link>
+                        <Link className={`nav-link ${currentPath === "/note" ? "active" : ""}`} to="/note">便签</Link>
                     </div>
                 </div>
-            </nav >
-        );
-    }
+            </div>
+        </nav >
+    );
 }
 
 export default Navbar;
