49ed82346d93e298c7ad83965bf5abd025d3c17f
ChrisKim <1515748548@qq.com>
2022-11-12 22:39:40 +0800

    [feat]: 便签编辑功能

--- src/page/noteDetail.jsx
+++ src/page/noteDetail.jsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import React, { Component } from 'react';
 import { Route, Routes, useParams } from 'react-router';
 import EditReminder from '../components/editReminder';
@@ -7,6 +8,7 @@ class NoteDetail extends Component {
     state = {
         reminderData: [],
         selectedData: {},
+        selectedIndex: 0,
     }
 
     constructor(props) {
@@ -14,7 +16,8 @@ class NoteDetail extends Component {
         const localStorageData = localStorage.getItem('reminderData');
         if (localStorageData) {
             this.state.reminderData = JSON.parse(localStorageData);
-            this.state.selectedData = this.state.reminderData.find(x => x.id === parseInt(props.params.id));
+            this.state.selectedIndex = this.state.reminderData.findIndex(x => x.id === parseInt(props.params.id));
+            this.state.selectedData = this.state.reminderData[this.state.selectedIndex];
         }
     }
 
@@ -27,13 +30,25 @@ class NoteDetail extends Component {
         window.history.back(-1);
     }
 
+    handleEdit = (title, content) => {
+        const reminderData = [...this.state.reminderData];
+        const selectedData = this.state.selectedData;
+        selectedData.title = title;
+        selectedData.content = content;
+        selectedData.date = dayjs().format("YYYY-MM-DD HH:mm");
+        this.setState({ selectedData });
+        reminderData[this.state.selectedIndex] = selectedData;
+        this.setState({ reminderData });
+        localStorage.setItem('reminderData', JSON.stringify(reminderData));
+    }
+
     render() {
         return (
             <React.Fragment>
                 <h1 className="mt-3 mb-3">我的便签</h1>
                 <Routes>
                     <Route path="/" element={<ShowReminder {...this.state.selectedData} handleDelete={this.handleDelete} />}></Route>
-                    <Route path="/edit" element={<EditReminder />}></Route>
+                    <Route path="/edit" element={<EditReminder {...this.state.selectedData} handleEdit={this.handleEdit} />}></Route>
                 </Routes>
             </React.Fragment>
         );
