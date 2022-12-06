078189c0a1184fe78bc66763ba607dabeada6581
ChrisKim <1515748548@qq.com>
2022-12-06 11:27:52 +0800

    [feat]: 登录和注册界面

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './LoginPage.scss';
+
 
 const LoginPage = () => {
   return (
-    <h1>Login Page</h1>
+    <div className="card p-5 div-login">
+      <h1 className="mb-5 text-center">登录</h1>
+      <div className="input-group mb-4">
+        <span className="input-group-text">账号</span>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="input-group mb-2">
+        <span className="input-group-text">密码</span>
+        <input type="password" className="form-control" />
+      </div>
+      <p className="text-end mb-3">没有账号？这就<Link to="/register">注册</Link></p>
+      <button type="button" className="btn btn-primary">
+        <i className="bi bi-rocket-takeoff me-2"></i>
+        登录！
+      </button>
+    </div>
   );
 }
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import './RegisterPage.scss';
 
 const RegisterPage = () => {
   return (
-    <h1>Register Page</h1>
+    <div className="card p-5 div-register">
+      <h1 className="mb-5 text-center">注册</h1>
+      <div className="input-group mb-4">
+        <span className="input-group-text">账号</span>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="input-group mb-4">
+        <span className="input-group-text">密码</span>
+        <input type="password" className="form-control" />
+      </div>
+      <div className="input-group mb-5">
+        <span className="input-group-text">确认</span>
+        <input type="password" className="form-control" />
+      </div>
+      <button type="button" className="btn btn-primary">
+        <i className="bi bi-rocket-takeoff me-2"></i>
+        注册！
+      </button>
+    </div>
   );
 }
 
