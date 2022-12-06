e4e01344023d25f23b0a9edf5345841292ce30d8
ChrisKim <1515748548@qq.com>
2022-12-06 12:35:50 +0800

    [feat]: 导航栏显示登录信息

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router';
 import icon from '../static/images/icon.png';
-
+import './NavBar.scss';
 
 const NavBar = () => {
   const currentPath = useLocation().pathname;
+
+  const handleLogout = () => {
+    localStorage.clear("uid");
+    localStorage.clear("username");
+    localStorage.clear("token");
+    window.location.reload();
+    window.location.href = "./login";
+  }
+
   return (
-    <nav className="navbar sticky-top navbar-expand navbar-dark bg-dark">
+    <nav className="navbar sticky-top navbar-expand navbar-dark bg-dark div-navbar">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <img src={icon} alt="icon" width="25" height="25" className="d-inline-block align-text-top"></img>
@@ -20,6 +29,18 @@ const NavBar = () => {
             <Link className={`nav-link ${localStorage.getItem("token") === null ? "disabled" : ""} ${currentPath.substring(0, 5) === "/note" ? "active" : ""}`} to="/note">便签</Link>
           </div>
         </div>
+        {localStorage.getItem("uid") === null ? (
+          <span className="navbar-text float-end">
+            <Link to="/login">登录</Link>
+            &nbsp;/&nbsp;
+            <Link to="/register">注册</Link>
+          </span>
+        ) : (
+          <span className="navbar-text float-end">
+            {`${localStorage.getItem("username")} (UID: ${localStorage.getItem("uid")})`}
+            <span className="ms-2 logout" onClick={handleLogout}>登出</span>
+          </span>
+        )}
       </div>
     </nav >
   );
